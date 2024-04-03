446526c716bd622d92526a284f3d1960cd4e730e
Emmett Rayes <emmett.rayes@icloud.com>
2024-04-03 21:59:58 +0200

    Add repl crate

---
+++ repl/src/main.rs
@@ -0,0 +1,21 @@
+use f_prime::expression::buffer::PositionedBuffer;
+use f_prime::expression::Expression;
+use f_prime::untyped_lambda::eval::by_value::CallByValueEvaluator;
+use f_prime::untyped_lambda::eval::BetaReduction;
+use f_prime::untyped_lambda::term::de_bruijn::DeBruijnConverter;
+use f_prime::untyped_lambda::term::pretty_print::UntypedPrettyPrinter;
+use f_prime::untyped_lambda::term::UntypedTerm;
+use std::io::BufRead;
+
+fn main() -> Result<(), std::io::Error> {
+    for line in std::io::stdin().lock().lines() {
+        let input = line?;
+        let buffer = PositionedBuffer::new(input.as_str());
+        let output = UntypedTerm::parse(buffer);
+        let term = DeBruijnConverter::convert(output.unwrap().0);
+        let value = CallByValueEvaluator::reduce(term);
+        let result = UntypedPrettyPrinter::format(value.unwrap());
+        println!("{}", result);
+    }
+    Ok(())
+}
